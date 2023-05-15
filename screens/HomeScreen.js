76cbfffe5070be6ca96427b7a9e77c86b1ca8954
Timothé <huarttim@gmail.com>
2023-05-15 16:16:54 +0200

    navigationbar

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,9 +6,15 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
+  View,
 } from "react-native";
 
 export default function HomeScreen({ navigation }) {
-  return <Text>yo</Text>;
+  return <View style={styles.container}></View>;
 }
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#E0CDA9",
+  },
+});
