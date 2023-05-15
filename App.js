6838ff797173fc4ee7d7bc79964fac90b232a220
Elouan Maurice <elouan.maurice@gmail.com>
2023-05-15 14:31:34 +0200

    frontelouan1

--- App.js
+++ App.js
@@ -3,7 +3,10 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -26,8 +29,8 @@ const TabNavigator = () => {
       tabBarInactiveTintColor: '#335561',
       headerShown: false,
     })}>
-      <Tab.Screen name="Map" component={MapScreen} />
-      <Tab.Screen name="Places" component={PlacesScreen} />
+      <Tab.Screen name="Home" component={HomeScreen} />
+      {/* <Tab.Screen name="Places" component={PlacesScreen} /> */}
     </Tab.Navigator>
   );
 };
@@ -36,7 +39,7 @@ export default function App() {
   return (
     <NavigationContainer>
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="TabNavigator" component={TabNavigator} />
     </Stack.Navigator>
   </NavigationContainer>
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -7,7 +7,35 @@ import {
     TextInput,
     TouchableOpacity,
   } from "react-native";
+
   
-  export default function HomeScreen({ navigation }) {
-    return <Text>yo</Text>;
+  export default function LoginScreen({ navigation }) {
+
+    const handleSubmit = () => {
+        // dispatch(updateNickname(nickname));
+        navigation.navigate('TabNavigator');
+      };
+
+    return (
+        <TouchableOpacity onPress={() => handleSubmit()} style={styles.button} activeOpacity={0.8}>
+            <Text style={styles.textButton}>Go to map</Text>
+        </TouchableOpacity>
+    );
   }
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    paddingTop: 8,
+    width: '80%',
+    marginTop: 30,
+    backgroundColor: '#ec6e5b',
+    borderRadius: 10,
+    marginBottom: 80,
+  },
+  textButton: {
+    color: '#ffffff',
+    height: 30,
+    fontWeight: '600',
+    fontSize: 16,
+  },
+});
