54fd10e432f53519d5fcb2b9e8a2b63dd6374733
alexsuire <alexandresuire75002@gmail.com>
2023-05-15 10:44:20 +0200

    homescreen

---
+++ screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+
+export default function HomeScreen({ navigation }) {
+  return <Text>yo</Text>;
+}
+
