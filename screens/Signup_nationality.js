2dcc10d963a7ae7fe31b3a3d28d7617fd573f789
alexsuire <alexandresuire75002@gmail.com>
2023-05-16 16:16:07 +0200

    yo

--- screens/Signup_nationality.js
+++ screens/Signup_nationality.js
@@ -12,9 +12,15 @@ import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import Favorite_spots_signup from "../components/Favorite_spots_signup";
 import Nationality from "../components/Nationality";
+import countries from "../data/countries.json"
 
 export default function Signup_nationality({ navigation }) {
   const [nationalities, setNationalities] = useState([]);
+  const [selectedNationality, setSelectedNationality] = useState('');
+  const [filteredNationalities, setFilteredNationalities] = useState([]);
+
+
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -24,7 +30,6 @@ export default function Signup_nationality({ navigation }) {
         );
         const json = await response.json();
         const data = json.data;
-        console.log("fetchNatio", data);
         setNationalities(data);
       } catch (error) {
         console.error(error);
@@ -33,12 +38,20 @@ export default function Signup_nationality({ navigation }) {
     fetchData();
   }, []);
 
-  console.log("nationalites", nationalities);
+  const handleSearch = (text) => {
+    const filtered = nationalities.filter((nation) =>
+      nation.country.toLowerCase().includes(text.toLowerCase())
+    );
+    setFilteredNationalities(filtered);
+    setSelectedNationality(text);
+  };
 
-//   const nation =
-//     nationalities.length > 0
-//       ? nationalities.map((data, i) => <Nationality key={i} {...data} />)
-//       : null;
+
+
+  const nation =
+  filteredNationalities.length > 0
+      ? filteredNationalities.map((data, i) => <Nationality key={i} {...data} />)
+      : null;
 
   return (
     <KeyboardAvoidingView
@@ -47,10 +60,17 @@ export default function Signup_nationality({ navigation }) {
     >
       <View style={styles.top}>
         <Text style={{ fontFamily: "Lato-Bold" }}>
-          Choose your nationality(ies)
+          Choose your nationality
         </Text>
+        <TextInput
+          style={styles.input}
+          value={selectedNationality}
+          placeholder="Enter your nationality"
+          onChangeText={handleSearch}
+
+        />
       </View>
-      <View style={styles.spots}></View>
+      <View style={styles.spots}>{nation}</View>
     </KeyboardAvoidingView>
   );
 }
