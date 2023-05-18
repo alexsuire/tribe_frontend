d415170d94e51bf9903048bb8af099fb3f46f42d
Timothé <huarttim@gmail.com>
2023-05-18 10:22:17 +0200

    pull

--- screens/SessionScreen.js
+++ screens/SessionScreen.js
@@ -6,8 +6,24 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
+  View,
 } from "react-native";
 
 export default function SessionScreen({ navigation }) {
-  return <Text>yo</Text>;
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}></View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#E0CDA9",
+  },
+  header: {
+    height: "30%",
+    backgroundColor: "#F0F0F0",
+  },
+});
