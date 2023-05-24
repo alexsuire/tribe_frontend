4118c786ece06d0f3c5b6caf8b82b9bf836d2554
Timothé <huarttim@gmail.com>
2023-05-24 09:18:38 +0200

    pull alex

--- myfetchapi.js
+++ myfetchapi.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 export default MY_FETCH_API = "http://10.33.210.185:3000";
-=======
-export default MY_FETCH_API = "http://10.33.210.186:3000";
->>>>>>> 96a11a8379aa9f3fe325730cbf3aaf96c8e8f21e
